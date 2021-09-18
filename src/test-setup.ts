d6aecfd7d7faa79cd4b7148d9c1d1d0428611ba1
jbristowe <john.bristowe@octopus.com>
2021-09-18 11:10:24 +1000

    fix: added missing input test values

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -3,13 +3,19 @@ import * as tmp from 'tmp'
 tmp.setGracefulCleanup()
 const tmpdir = tmp.dirSync({template: 'run-runbook-XXXXXX'})
 process.env = Object.assign(process.env, {
+  GITHUB_ACTION: '1',
   INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
-  INPUT_SERVER: process.env['OCTOPUS_URL'],
+  INPUT_CANCEL_ON_TIMEOUT: false,
+  INPUT_DEBUG: false,
   INPUT_ENVIRONMENTS: 'Webinar Environment',
+  INPUT_FORCE_PACKAGE_DOWNLOAD: false,
+  INPUT_IGNORE_SSL_ERRORS: false,
+  INPUT_NO_RAW_LOG: false,
   INPUT_PROJECT: 'Projects-6445',
   INPUT_RUNBOOK: 'Runbooks-701',
+  INPUT_SERVER: process.env['OCTOPUS_URL'],
   INPUT_SHOW_PROGRESS: 'true',
+  INPUT_WAIT_FOR_RUN: false,
   RUNNER_TEMP: tmpdir.name,
-  RUNNER_TOOL_CACHE: tmpdir.name,
-  GITHUB_ACTION: '1'
+  RUNNER_TOOL_CACHE: tmpdir.name
 })
