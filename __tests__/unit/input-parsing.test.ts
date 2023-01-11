66ca80459302d7e9408c69311347101cee16a3dc
Shannon Lewis <slewis74@gmail.com>
2023-01-11 12:18:46 +1000

    chore: fixed test

--- __tests__/unit/input-parsing.test.ts
+++ __tests__/unit/input-parsing.test.ts
@@ -7,6 +7,6 @@ test('get input parameters', () => {
   expect(inputParameters.environments[0]).toBe('Dev')
   expect(inputParameters.environments[1]).toBe('Staging')
   expect(inputParameters.variables).toBeDefined()
-  expect(inputParameters.variables?.get('foo')).toBe('quux')
-  expect(inputParameters.variables?.get('bar')).toBe('xyzzy')
+  expect(inputParameters.variables?.['foo']).toBe('quux')
+  expect(inputParameters.variables?.['bar']).toBe('xyzzy')
 })
